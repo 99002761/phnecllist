5872f8ab8412c9a975aff4985d69a03255c8229e
Srikar Reddy <69463316+stepin104826@users.noreply.github.com>
2020-11-12 14:03:26 +0530

    shared prefernces,databases

---
+++ app/src/main/java/com/example/myapp/database/DbAccessObj.java
@@ -0,0 +1,63 @@
+package com.example.myapp.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DbAccessObj {
+    SQLiteDatabase database;
+    DbHelper dbHelper;
+    public DbAccessObj(Context context) {
+        dbHelper = new DbHelper(context);
+    }
+    public void openDb(){
+        database = dbHelper.getWritableDatabase();
+
+    }
+    private void closeDb(){}
+
+    private void createRow(){}
+    public void createRow(String title, String subtitle){
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,title);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE,subtitle);
+        database.insert(FeedReaderContract.FeedEntry.TABLE_NAME,null,values);
+    }
+    public String readRow(){
+        //query my db/table
+        //database.rawQuery("select * from notes_table",null);
+        Cursor cursor = database.query(FeedReaderContract.FeedEntry.TABLE_NAME,null,null,null,null,null,null);
+        //return the result as a string
+        cursor.moveToLast();
+        int titleIndex = cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE);
+        int subtitleIndex = cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE);
+
+        String title = cursor.getString(titleIndex);
+        String subtitle = cursor.getString(subtitleIndex);
+        return title +"\n" +subtitle;
+    }
+    private void updateRow(){}
+    private void deleteRow(){}
+    public Cursor getRows() {
+        Cursor cursor = database.query(FeedReaderContract.FeedEntry.TABLE_NAME,null,null,null,null,null,null);
+        return cursor;
+    }
+
+    public String query(String queryParam) {
+        String table = FeedReaderContract.FeedEntry.TABLE_NAME;
+        String[] columns = {FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE}; //projection = columns
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE +" =?"; //selection = rows
+        String[] selectionArgs = {queryParam};
+        String groupBy = null;
+        String having = null;
+        String orderBy = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE+" DESC";
+        String limit = "10";
+
+        Cursor cursor = database.query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+        int subtitleIndex = cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE);
+        cursor.moveToLast();
+        String subtitle = cursor.getString(subtitleIndex);
+        return subtitle;
+    }
+}
